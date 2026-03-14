e2dde73d0f21dead04b0b15747849650b9bbdebc
blackey0927 <blackey0927@gmail.com>
2026-03-14 22:58:04 +0800

    Add service worker for caching resources

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'ops-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // 優先讀取快取，若無則透過網路抓取
+        return response || fetch(event.request);
+      })
+  );
+});
